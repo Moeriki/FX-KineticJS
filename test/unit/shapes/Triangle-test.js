c8432e4e01396c1e8f1e7cd307de7ec78d52f598
nteirlin <nicolas@xaop.com>
2014-02-11 14:16:27 +0100

    Removing negative height, using scale to mirror isosceles triangle

--- test/unit/shapes/Triangle-test.js
+++ test/unit/shapes/Triangle-test.js
@@ -16,14 +16,15 @@ suite('Triangle', function(){
     });
 
     var triangle1 = new Kinetic.TriangleIsosceles({
-        x: 100,
+        x: 110,
         y: 115,
         fill: 'blue',
         stroke: 'red',
         strokeWidth: 4,
-        height: -100,
+        height: 100,
         width: 50,
-        draggable: true
+        draggable: true,
+        scale: {x: -1, y: -1}
     });
 
     var triangle2 = new Kinetic.TriangleIsosceles({
@@ -47,7 +48,7 @@ suite('Triangle', function(){
     assert.equal(triangle1.getClassName(), 'TriangleIsosceles');
     assert.equal(triangle2.getClassName(), 'TriangleIsosceles');
 
-    assert.equal(triangle0.getHeight(), - triangle1.getHeight());
+    assert.equal(triangle0.getHeight(), triangle1.getHeight());
     assert.equal(triangle0.getWidth(), triangle1.getWidth());
 
     assert.equal(triangle0.getWidth(), triangle2.getWidth());
@@ -55,6 +56,7 @@ suite('Triangle', function(){
     assert.equal(triangle0.getHeight() + 50, triangle2.getHeight());
 
     var trace = layer.getContext().getTrace();
-    assert.equal(trace, 'clearRect(0,0,578,200);clearRect(0,0,578,200);save();transform(1,0,0,1,50,20);beginPath();moveTo(0,0);lineTo(-25,100);lineTo(25,100);lineTo(0,0);closePath();fillStyle=yellow;fill();lineWidth=4;strokeStyle=red;stroke();restore();save();transform(1,0,0,1,100,115);beginPath();moveTo(0,0);lineTo(-25,-100);lineTo(25,-100);lineTo(0,0);closePath();fillStyle=blue;fill();lineWidth=4;strokeStyle=red;stroke();restore();save();transform(1,0,0,1,170,20);beginPath();moveTo(0,0);lineTo(-25,150);lineTo(25,150);lineTo(0,0);closePath();fillStyle=red;fill();lineWidth=4;strokeStyle=yellow;stroke();restore();');
+    console.log(trace);
+    assert.equal(trace, 'clearRect(0,0,578,200);clearRect(0,0,578,200);save();transform(1,0,0,1,50,20);beginPath();moveTo(0,0);lineTo(-25,100);lineTo(25,100);lineTo(0,0);closePath();fillStyle=yellow;fill();lineWidth=4;strokeStyle=red;stroke();restore();save();transform(-1,0,0,-1,110,115);beginPath();moveTo(0,0);lineTo(-25,100);lineTo(25,100);lineTo(0,0);closePath();fillStyle=blue;fill();lineWidth=4;strokeStyle=red;stroke();restore();save();transform(1,0,0,1,170,20);beginPath();moveTo(0,0);lineTo(-25,150);lineTo(25,150);lineTo(0,0);closePath();fillStyle=red;fill();lineWidth=4;strokeStyle=yellow;stroke();restore();');
   });
 });
