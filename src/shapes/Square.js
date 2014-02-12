706da3a11c9b8a015ac4255ea9773f76977c8b30
nteirlin <nicolas@xaop.com>
2014-02-12 11:34:02 +0100

    Removing lock ratio, resize is via scale

--- src/shapes/Square.js
+++ src/shapes/Square.js
@@ -29,33 +29,15 @@
         // implements Shape.prototype.setWidth()
         setWidth: function(width) {
             Kinetic.Node.prototype.setWidth.call(this, width);
-            if(this.lockRatio()){this._setAttr('height', width);}
+            this._setAttr('height', width);
         },
         // implements Shape.prototype.setHeight()
         setHeight: function(height) {
             Kinetic.Node.prototype.setHeight.call(this, height);
-            if(this.lockRatio()){this._setAttr('width', height);}
+            this._setAttr('width', height);
         }
     };
 
     Kinetic.Util.extend(Kinetic.Square, Kinetic.Rect);
     Kinetic.Collection.mapMethods(Kinetic.Square);
-
-    // add getters setters
-    Kinetic.Factory.addGetterSetter(Kinetic.Square, 'lockRatio', true);
-
-    /**
-     * get/set ratio lock, ensures that sides square stays equal
-     * @name lockRatio
-     * @method
-     * @memberof Kinetic.TriangleEquilateral.prototype
-     * @param {Boolean} lockRatio
-     * @returns {Boolean}
-     * @example
-     * // get ratio lock<br>
-     * var lock = square.lockRatio();<br><br>
-     *
-     * // set ratio lock<br>
-     * square.lockRatio(false);<br>
-     */
 })();
