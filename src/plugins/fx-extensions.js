ad13743e93768c40c558c8950c095bc3f9f879dc
Dieter Luypaert <dieterluypaert@gmail.com>
2014-02-24 14:35:15 +0100

    add documentation template

--- src/plugins/fx-extensions.js
+++ src/plugins/fx-extensions.js
@@ -16,23 +16,33 @@
 
     Kinetic.Util.addMethods(Kinetic.Node, {
 
+        /**
+        */
         isFirst: function() {
             return this.index === 0;
         },
 
+        /**
+        */
         isLast: function() {
             return this.index === this.parent.children.length - 1;
         },
 
+        /**
+        */
         isOrphan: function() {
             return !this.parent;
         },
 
+        /**
+        */
         moveBefore: function(node) {
             this.moveTo(node.parent);
             this.setZIndex(node.index);
         },
 
+        /**
+        */
         moveAfter: function(node) {
             this.moveTo(node.parent);
             this.setZIndex(node.index + 1);
