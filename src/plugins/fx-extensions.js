22a19a0a45ad87f098e5b6ddf72679fd0766bebd
Dieter Luypaert <dieterluypaert@gmail.com>
2014-03-05 14:18:12 +0100

    moved some functions to fx-kizzle for filtering support

--- src/plugins/fx-extensions.js
+++ src/plugins/fx-extensions.js
@@ -48,34 +48,6 @@
             this.setZIndex(node.index + 1);
         },
 
-        /**
-        * Get the next node from its siblings. Returns undefined if this node is the last.
-        * @method
-        * @memberof Kinetic.Node.prototype
-        * @returns {Kinetic.Node}
-        */
-        next: function() {
-            return this.parent.children[this.index + 1];
-        },
-
-        /**
-        * Get the previous node from its siblings. Returns undefined if this node is the first.
-        * @method
-        * @memberof Kinetic.Node.prototype
-        * @returns {Kinetic.Node}
-        */
-        previous: function() {
-            return this.parent.children[this.index - 1];
-        },
-
-        /**
-        * Get all nodes that have the same parent as the current {@link Kinetic.Node}, (include the node itself.
-        * @returns {[Kinetic.Node]}
-        */
-        siblings: function() {
-            return this.parent.children;
-        },
-
     });
 
 })();
