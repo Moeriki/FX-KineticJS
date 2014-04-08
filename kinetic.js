38c1113a0e91e881af757e00282b062b83943125
Ruben Tytgat <ruben@xaop.com>
2014-04-08 11:26:06 +0200

    Rebuild

--- kinetic.js
+++ kinetic.js
@@ -4,7 +4,7 @@
  * http://www.kineticjs.com/
  * Copyright 2013, Eric Rowell
  * Licensed under the MIT or GPL Version 2 licenses.
- * Date: 2014-03-28
+ * Date: 2014-04-08
  *
  * Copyright (C) 2011 - 2013 by Eric Rowell
  *
@@ -750,6 +750,22 @@ var Kinetic = {};
                 xt = (x - m4 - (m2 * yt)) / m0;
 
             this.translate(xt, yt);
+        },
+        transformCoords: function(x, y) {
+            return {
+                x: this.m[0] * x + this.m[2] * y + this.m[4],
+                y: this.m[1] * x + this.m[3] * y + this.m[5]
+            };
+        },
+
+        transformPoint: function(p) {
+            return this.transformCoords(p.x, p.y);
+        },
+
+        dup: function() {
+            var t = new Kinetic.Transform();
+            t.m = this.m.slice(0);
+            return t;
         }
     };
 
@@ -3868,6 +3884,65 @@ var Kinetic = {};
             this.drawScene();
             this.drawHit();
             return this;
+        },
+        /**
+         * Calculate the bounding box within the node's local space.
+         *
+         * A bare node's bounding box can be calculated by simply using the w/h
+         * This may be overridden for irregular shapes like circles.
+         */
+        calculateLocalBoundingBox: function () {
+            return {
+                left: 0,
+                top: 0,
+                right: this.getWidth(),
+                bottom: this.getHeight()
+            };
+        },
+
+        /*
+         * Calculates the bounding box in the node's parent space.
+         *
+         * This may be overridden for more complex shapes to add a more correct bounding box.
+         * For example, a rotated circle's bounding box should not rotate, since it
+         * always has the same radius.
+         */
+        calculateBoundingBox: function () {
+            var transform = this.getTransform();
+            var localBounds = this.calculateLocalBoundingBox();
+
+            // Original bounding box is a 0x0 rectangle centered on our position
+            var res = { left: this.getX(), top: this.getY(), right: this.getX(), bottom: this.getY() };
+
+            // Make corners from the local bounds
+            var localCorners = [{
+                x: localBounds.left,
+                y: localBounds.top
+            }, {
+                x: localBounds.right,
+                y: localBounds.top
+            }, {
+                x: localBounds.right,
+                y: localBounds.bottom
+            }, {
+                x: localBounds.left,
+                y: localBounds.bottom
+            }];
+
+            // Now transform those corners from our local space into our parent's space.
+            var cornersInParentSpace = localCorners.map(function (corner) {
+                return transform.transformPoint(corner);
+            });
+
+            // Push the bounds of the bounding-box-up-till-now
+            cornersInParentSpace.forEach(function (corner) {
+                res.left   = Math.min(res.left, corner.x);
+                res.top    = Math.min(res.top, corner.y);
+                res.right  = Math.max(res.right, corner.x);
+                res.bottom = Math.max(res.bottom, corner.y);
+            });
+
+            return res;
         }
     });
 
@@ -8656,6 +8731,30 @@ var Kinetic = {};
             if (type !== 'Group' && type !== 'Shape') {
                 Kinetic.Util.error('You may only add groups and shapes to groups.');
             }
+        },
+        /**
+         * Overrides the default local bounding box calculation with
+         * one the incorporates all its children that are visible
+         */
+        calculateLocalBoundingBox: function () {
+            // Start with no bounds
+            var res = { left: 0, top: 0, right: 0, bottom: 0 };
+
+            // Iterate over all children
+            this.children.filter(function (child) {
+                return child.getVisible();
+            }).forEach(function (child) {
+                // Get the child's bounding box (may recurse back into this function if the child is a group)
+                var childBounds = child.calculateBoundingBox();
+
+                // Push the bounds of the bounding-box-up-till-now
+                res.left   = Math.min(res.left, childBounds.left);
+                res.top    = Math.min(res.top, childBounds.top);
+                res.right  = Math.max(res.right, childBounds.right);
+                res.bottom = Math.max(res.bottom, childBounds.bottom);
+            });
+
+            return res;
         }
     });
     Kinetic.Util.extend(Kinetic.Group, Kinetic.Container);
